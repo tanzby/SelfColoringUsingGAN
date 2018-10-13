f3c8bf765626a7c20772fbed8af4e5c770aac1e0
tanzby <tanzby@outlook.com>
2018-10-13 17:51:09 +0800

    guodongpeng 增加读取文件功能

--- DataProcessing/opencvTest.py
+++ DataProcessing/opencvTest.py
@@ -11,3 +11,33 @@ def file_name(file_dir):
                 file_list.append(os.path.join(root, file))
     return file_list
 
+
+def get_test_image_list():
+    bg_by_user_test_list = file_name("../data/bg_by_user/test")
+    image_list = []
+    for file in bg_by_user_test_list:
+        sub_image_list = []
+        sub_image_list.append(file)
+        temp_file = file.replace("../data/bg_by_user/test", "../data/foreground/test")
+        sub_image_list.append(temp_file)
+        temp_file = file.replace("../data/bg_by_user/test", "../data/inner_mask/test")
+        sub_image_list.append(temp_file)
+        image_list.append(sub_image_list)
+    return image_list
+
+
+def get_train_image_list():
+    bg_by_user_train_list = file_name("../data/bg_by_user/train")
+    image_list = []
+    for file in bg_by_user_train_list:
+        sub_image_list = []
+        sub_image_list.append(file)
+        temp_file = file.replace("../data/bg_by_user/train", "../data/foreground/train")
+        sub_image_list.append(temp_file)
+        temp_file = file.replace("../data/bg_by_user/train", "../data/inner_mask/train")
+        sub_image_list.append(temp_file)
+        image_list.append(sub_image_list)
+    return image_list
+
+l = get_train_image_list()
+print(l)
